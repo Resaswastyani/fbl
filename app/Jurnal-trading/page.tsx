4baf79c783b61f6a97a6ac8635ff3559c9445fba
Resaswastyani <resaarrazy@gmail.com>
2026-06-04 15:21:47 +0700

    perbaikan jurnal trading1

--- app/Jurnal-trading/page.tsx
+++ app/Jurnal-trading/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { TradingJournalPage } from "@/components/TradingJurnalPage";
 
-export default function JournalPage() {
+export default function JurnalTradingPage() {
   return <TradingJournalPage />;
 }

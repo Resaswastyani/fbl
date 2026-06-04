e7b2e567c00c54706c540586cf541a4f3a2ddec8
Resaswastyani <resaarrazy@gmail.com>
2026-06-04 16:29:28 +0700

    perbaikan routing

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,12 @@
+// import createMiddleware from "next-intl/middleware";
+// import { routing } from "./i18n/routing";
+
+// export default createMiddleware(routing);
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.).*)"],
+// };
+
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   typescript: {
+// //     ignoreBuildErrors: true,
+// //   },
+// //   images: {
+// //     unoptimized: true,
+// //   },
+// // }
+
+// // export default nextConfig
+
+// import createNextIntlPlugin from "next-intl/plugin";
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   typescript: {
@@ -6,22 +20,19 @@
 //   images: {
 //     unoptimized: true,
 //   },
-// }
+// };
 
-// export default nextConfig
+// const withNextIntl = createNextIntlPlugin("./i18n/i18n.ts");
+
+// export default withNextIntl(nextConfig);
 
 import createNextIntlPlugin from "next-intl/plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 };
 
 const withNextIntl = createNextIntlPlugin("./i18n/i18n.ts");
-
 export default withNextIntl(nextConfig);

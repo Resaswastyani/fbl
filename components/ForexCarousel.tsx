baa024cd8753f9ec0f19c010d33e829c29ba00d3
Resaswastyani <resaarrazy@gmail.com>
2026-07-23 15:13:19 +0700

    perbaikan visi

--- components/ForexCarousel.tsx
+++ components/ForexCarousel.tsx
@@ -12,33 +12,33 @@ function ForexCarousel() {
       // If script is there but no iframe, maybe it failed. Let's clear and retry.
       container.current.innerHTML = '<div class="tradingview-widget-container__widget"></div>';
     }
-    
+
     const script = document.createElement("script");
     script.src = "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
     script.type = "text/javascript";
     script.async = true;
     script.innerHTML = JSON.stringify({
       symbols: [
-        { proName: "FX:EURUSD", title: "EUR/USD" },
-        { proName: "FX:GBPUSD", title: "GBP/USD" },
-        { proName: "FX:USDJPY", title: "USD/JPY" },
-        { proName: "FX:AUDUSD", title: "AUD/USD" },
-        { proName: "FX:USDCHF", title: "USD/CHF" },
-        { proName: "FX:NZDUSD", title: "NZD/USD" },
-        { proName: "FX:USDCAD", title: "USD/CAD" },
-        { proName: "FX:EURGBP", title: "EUR/GBP" },
-        { proName: "FX:EURJPY", title: "EUR/JPY" },
-        { proName: "FX:GBPJPY", title: "GBP/JPY" },
-        { proName: "FX:CHFJPY", title: "CHF/JPY" },
-        { proName: "FX:AUDJPY", title: "AUD/JPY" },
-        { proName: "FX:NZDJPY", title: "NZD/JPY" },
-        { proName: "FX:EURCHF", title: "EUR/CHF" },
-        { proName: "FX:GBPCHF", title: "GBP/CHF" },
-        { proName: "FX:CADJPY", title: "CAD/JPY" },
-        { proName: "FX:AUDCAD", title: "AUD/CAD" },
-        { proName: "FX:EURCAD", title: "EUR/CAD" },
-        { proName: "FX:GBPCAD", title: "GBP/CAD" },
-        { proName: "FX:USDSEK", title: "USD/SEK" }
+        { proName: "FX:EURUSD", title: "EURUSD" },
+        { proName: "FX:GBPUSD", title: "GBPUSD" },
+        { proName: "FX:USDJPY", title: "USDJPY" },
+        { proName: "FX:AUDUSD", title: "AUDUSD" },
+        { proName: "FX:USDCHF", title: "USDCHF" },
+        { proName: "FX:NZDUSD", title: "NZDUSD" },
+        { proName: "FX:USDCAD", title: "USDCAD" },
+        { proName: "FX:EURGBP", title: "EURGBP" },
+        { proName: "FX:EURJPY", title: "EURJPY" },
+        { proName: "FX:GBPJPY", title: "GBPJPY" },
+        { proName: "FX:CHFJPY", title: "CHFJPY" },
+        { proName: "FX:AUDJPY", title: "AUDJPY" },
+        { proName: "FX:NZDJPY", title: "NZDJPY" },
+        { proName: "FX:EURCHF", title: "EURCHF" },
+        { proName: "FX:GBPCHF", title: "GBPCHF" },
+        { proName: "FX:CADJPY", title: "CADJPY" },
+        { proName: "FX:AUDCAD", title: "AUDCAD" },
+        { proName: "FX:EURCAD", title: "EURCAD" },
+        { proName: "FX:GBPCAD", title: "GBPCAD" },
+        { proName: "FX:USDSEK", title: "USDSEK" }
       ],
       showSymbolLogo: true,
       isTransparent: true,
@@ -46,7 +46,7 @@ function ForexCarousel() {
       colorTheme: "dark",
       locale: "id"
     });
-    
+
     if (container.current) {
       container.current.appendChild(script);
     }
